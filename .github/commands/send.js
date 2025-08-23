32dfe451dc7095237fa281e700391e5748b4dfc5
extremeheat <extreme@protonmail.ch>
2025-08-23 16:42:30 -0400

    Add workflow to send artifacts back to minecraft-data (#51)
    * add a send script
    
    * update
    
    * fix marker

---
+++ .github/commands/send.js
@@ -0,0 +1,24 @@
+const gh = require('gh-helpers')()
+
+module.exports = async ([mergedArtifactURL], helpers) => {
+  // Have minecraft-data handle this successful update
+  const dispatchPayload = {
+    owner: 'PrismarineJS',
+    repo: 'minecraft-data',
+    workflow: 'handle-mcpc-generator.yml',
+    branch: 'main',
+    inputs: {
+      versions: JSON.stringify(require('../../versions.json')),
+      mergedArtifactURL: mergedArtifactURL,
+      prNumber: process.env.PR_NUMBER
+    }
+  }
+  console.log('Sending workflow dispatch', dispatchPayload)
+  await gh.sendWorkflowDispatch(dispatchPayload)
+}
+
+if (require.main === module) {
+  const merged = process.env.ARTIFACT_URL
+  console.log('Handling merged artifacts @', merged)
+  module.exports([merged], { reply: console.log })
+}
